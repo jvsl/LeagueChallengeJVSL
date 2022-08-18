9a46d51065d013921bbf654c90f8e91f44988c52
João Victor <ccjoaovictor@gmail.com>
2022-08-18 12:43:49 -0300

    some UIKit extension to help

--- LeagueMobileChallengePairing/LeagueMobileChallenge/LeagueSocialMediaScene/Extensions/Extensions.swift
+++ LeagueMobileChallengePairing/LeagueMobileChallenge/LeagueSocialMediaScene/Extensions/Extensions.swift
@@ -1,9 +1,33 @@
-//
-//  Extensions.swift
-//  LeagueMobileChallenge
-//
-//  Created by joão lucas on 17/08/22.
-//  Copyright © 2022 Kelvin Lau. All rights reserved.
-//
+import UIKit
 
-import Foundation
+extension UIStackView {
+    func addArrangedSubviews(_ views: UIView...) {
+        views.forEach(addArrangedSubview(_:))
+    }
+}
+
+protocol ReusableView: AnyObject {
+    static var reuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var reuseIdentifier: String {
+        return NSStringFromClass(self)
+    }
+}
+
+protocol ViewCoding {
+    func addSubviews()
+    func addConstraints()
+    func additionalConfig()
+    func buildView()
+}
+
+extension ViewCoding {
+    func buildView() {
+        addSubviews()
+        addConstraints()
+        additionalConfig()
+    }
+}
+
