61f0b9115830d7b94265b00a395d502b8a36f07d
João Victor <ccjoaovictor@gmail.com>
2022-08-17 22:52:35 -0300

    only necessary data to parse in User model

--- LeagueMobileChallengePairing/LeagueMobileChallenge/LeagueSocialMediaScene/Models/User.swift
+++ LeagueMobileChallengePairing/LeagueMobileChallenge/LeagueSocialMediaScene/Models/User.swift
@@ -3,19 +3,5 @@
 struct User: Codable {
     let id: Int
     let avatar: String
-    let name, username, email: String
-    let address: Address
-}
-
-// MARK: - Address
-
-struct Address: Codable {
-    let street, suite, city, zipcode: String
-    let geo: Geo
-}
-
-// MARK: - Geo
-
-struct Geo: Codable {
-    let lat, lng: String
+    let username: String
 }
