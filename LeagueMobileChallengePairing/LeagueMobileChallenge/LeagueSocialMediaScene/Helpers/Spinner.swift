37c7f3be1aabf46c36f1f08b5daafabb2cd11343
João Victor <ccjoaovictor@gmail.com>
2022-08-18 12:44:06 -0300

    a loading view

--- LeagueMobileChallengePairing/LeagueMobileChallenge/LeagueSocialMediaScene/Helpers/Spinner.swift
+++ LeagueMobileChallengePairing/LeagueMobileChallenge/LeagueSocialMediaScene/Helpers/Spinner.swift
@@ -1,9 +1,42 @@
-//
-//  Spinner.swift
-//  LeagueMobileChallenge
-//
-//  Created by joão lucas on 18/08/22.
-//  Copyright © 2022 Kelvin Lau. All rights reserved.
-//
+import UIKit
 
-import Foundation
+open class LLSpinner {
+    internal static var spinnerView: UIActivityIndicatorView?
+    
+    public static var style: UIActivityIndicatorView.Style = .medium
+    public static var backgroundColor: UIColor = UIColor(white: 0, alpha: 0.6)
+    
+    internal static var touchHandler: (() -> Void)?
+    
+    public static func spin(style: UIActivityIndicatorView.Style = style, backgroundColor: UIColor = backgroundColor, touchHandler: (() -> Void)? = nil) {
+        if spinnerView == nil,
+            let window = UIApplication.shared.keyWindow {
+            let frame = UIScreen.main.bounds
+            spinnerView = UIActivityIndicatorView(frame: frame)
+            spinnerView?.backgroundColor = backgroundColor
+            spinnerView?.style = style
+            window.addSubview(spinnerView!)
+            spinnerView?.startAnimating()
+        }
+        
+        if touchHandler != nil {
+            self.touchHandler = touchHandler
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(runTouchHandler))
+            spinnerView?.addGestureRecognizer(tapGestureRecognizer)
+        }
+    }
+    
+    @objc internal static func runTouchHandler() {
+        if touchHandler != nil {
+            touchHandler?()
+        }
+    }
+    
+    public static func stop() {
+        if let _ = spinnerView {
+            spinnerView?.stopAnimating()
+            spinnerView?.removeFromSuperview()
+            spinnerView = nil
+        }
+    }
+}
